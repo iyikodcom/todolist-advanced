d0b0445acd28ad0b8313b8648a879be7c8423168
iyikodcom <tamertarkan@gmail.com>
2022-07-21 16:31:07 +0300

    0.1.0
    -README.md güncellendi
    -favicon eklendi
    -index.html'de geliştirmeler yapıldı

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# todolist-advanced
- 
+
+# Todolist-advanced
+
+Tarayıcıların sahip olduğu "localStorage" özelliği ile birlikte çalışan gelişmiş özelliklere sahip Todolist uygulaması. 
+
+[![bootstrap](https://img.shields.io/badge/poweredBy-iyikod.com-66CC00)](https://www.iyikod.com)
+
+
+## Kullanılan Frameworkler ve Kütüphaneler
+
+
+[![bootstrap](https://img.shields.io/badge/Bootstrap-v5.1.3-7952b3)](https://getbootstrap.com/docs/5.1/getting-started/introduction/)
+
+[![bootstrap icons](https://img.shields.io/badge/BootstrapIcons-v1.8.3-7952b3)](https://icons.getbootstrap.com/)
+
+[![jquery](https://img.shields.io/badge/Jquery-v3.6.0-0769ad)](https://www.jsdelivr.com/package/npm/jquery)
+
+  
+## Özellikler
+
+- Responsive tasarım
+- Sınızsız yapılacak listesi ekleme (localStorage kapasitesi kadar)
+- Yapılacak listelerine sınırsız madde ekleme (localStorage kapasitesi kadar)
+- Maddelleri onaylama, görüntüleme, düzenleme ve silme
+- Toplu bir şekilde listeleri ve maddelleri silme
+- Listeleri ve maddelerini dışa aktarma (JSON formatında)
+- Listeleri ve maddelerini içe aktarma (JSON formatında)
+- Dil seçeneği (Türkçe ve İngilizce)
+
+  
+## Örnek JSON Formatı (İçe Aktarma işlem için)
+
+```
+[
+  {
+    "name": "Liste1",
+    "todos": [
+      {
+        "value": "Madde1",
+        "status": true
+      },
+      {
+        "value": "Madde2",
+        "status": false
+      },
+      {
+        "value": "Madde3",
+        "status": true
+      },
+      {
+        "value": "Madde4",
+        "status": false
+      }
+    ]
+  },
+  {
+    "name": "Liste2",
+    "todos": [
+      {
+        "value": "Madde1",
+        "status": true
+      }
+    ]
+  },
+  {
+    "name": "Liste3",
+    "todos": [
+      {
+        "value": "Madde1",
+        "status": false
+      },
+      {
+        "value": "Madde2",
+        "status": false
+      },
+      {
+        "value": "Madde3",
+        "status": true
+      }
+    ]
+  }
+]
+```
+
+  
+## Lisans
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://choosealicense.com/licenses/mit/)
--- js/language.js
+++ js/language.js
@@ -4,25 +4,25 @@ const languageFlags = [
 ];
 
 const languages = [
-    {tr: 'Yeni Yapılacaklar Listesi Oluştur', en: 'Create New Todo List'},//->0
+    {tr: 'Yeni Yapılacak Listesi Oluştur', en: 'Create New Todo List'},//->0
     {tr: 'Todolist\'in adını girin...', en: 'Enter Todolist\'s name...'},//->1
     {tr: 'Oluştur', en: 'Create'},//->2
-    {tr: 'Yapılacaklar Listeleri', en: 'Todo Lists'},//->3
+    {tr: 'Yapılacak Listeleri', en: 'Todo Lists'},//->3
     {tr: 'İçe Aktar', en: 'Import'},//->4
     {tr: 'Dışa Aktar', en: 'Export'},//->5
     {tr: 'Tüm Listeleri Sil', en: 'Delete All Lists'},//->6
     {tr: 'Tüm Yapılacakları Sil', en: 'Delete All Todos'},//->7
-    {tr: 'Yapılacakları Ekle', en: 'Add Todo'},//->8
+    {tr: 'Yapılacak İçeriği Ekle', en: 'Add Todo'},//->8
     {tr: 'Ekle', en: 'Add'},//->9
     {tr: 'Kapat', en: 'Close'},//->10
-    {tr: 'Yapılacakları girin...', en: 'Enter todo...'},//->11
+    {tr: 'Yapılacak içeriği giriniz...', en: 'Enter todo...'},//->11
     {tr: 'Detaylar', en: 'Details'},//->12
     {tr: 'Güncelle', en: 'Update'},//->13
-    {tr: 'JSON girin...', en: 'Enter JSON...'},//->14
-    {tr: '<b>İpucu:</b> Yapılacaklar listesi bulunamadı !!! Lütfen yeni bir yapılacaklar listesi oluşturun.', en: '<b>Hint:</b> Todo list not found !!! Please create a new todo list.'},//->15
-    {tr: '<b>İpucu:</b> Yapılacak bulunamadı !!! Lütfen yapılacakları ekleyin.', en: '<b>Hint:</b> Todo not found !!! Please add todo.'},//--16
-    {tr: '<b>Uyarı:</b> Todolist\'in adını girmeniz gerekiyor !!!', en: '<b>Alert:</b> You need to enter todolist\'s name !!!'},//->17
-    {tr: '<b>Uyarı:</b> Yapılacakları girin !!!', en: '<b>Alert:</b> Enter todo !!!'},//->18
-    {tr: '<b>Uyarı:</b> JSON girin !!!', en: '<b>Alert:</b> Enter JSON !!!'},//->19
+    {tr: 'JSON giriniz...', en: 'Enter JSON...'},//->14
+    {tr: '<b>İpucu:</b> Yapılacak listesi bulunamadı !!! Lütfen yeni bir yapılacak listesi oluşturun.', en: '<b>Hint:</b> Todo list not found !!! Please create a new todo list.'},//->15
+    {tr: '<b>İpucu:</b> Yapılacak bulunamadı !!! Lütfen yapılacak içeriği ekleyin.', en: '<b>Hint:</b> Todo not found !!! Please add todo.'},//--16
+    {tr: '<b>Uyarı:</b> Todolist\'in adını giriniz !!!', en: '<b>Alert:</b> You need to enter todolist\'s name !!!'},//->17
+    {tr: '<b>Uyarı:</b> Yapılacak içeriği giriniz !!!', en: '<b>Alert:</b> Enter todo !!!'},//->18
+    {tr: '<b>Uyarı:</b> JSON giriniz !!!', en: '<b>Alert:</b> Enter JSON !!!'},//->19
     {tr: '<b>Uyarı:</b> JSON biçimi yanlış !!!', en: '<b>Alert:</b> JSON format is incorrect !!!'}//->20
 ];
